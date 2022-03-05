5c4234ff152891efc0a3a1eeba7234413ac76fa5
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-03-05 10:51:46 +0530

    port updated

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("*", (req, res) => {
   res.sendStatus(404).send("Not Found");
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
 });
 
