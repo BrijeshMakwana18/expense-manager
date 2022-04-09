6c1b634f56772eef9847c8c53ff4b2f96b4636d8
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-04-09 10:41:51 +0530

    app reducer added

--- client/src/screens/Home/reducer.js
+++ client/src/screens/Home/reducer.js
@@ -1,13 +1,15 @@
 import {actionTypes} from './actionTypes';
 const initialState = {
   userDetails: {},
+  dashboardData: {},
 };
 
 const AppReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_USER_DATA:
-      console.log(action);
       return {...state, userDetails: action.payload};
+    case actionTypes.SET_DASHBOARD_DATA:
+      return {...state, dashboardData: action.payload};
     default:
       return state;
   }
