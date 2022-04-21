6efac6485db1a2bdd14771ebe6201117a46f2a5d
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-04-21 19:53:43 +0530

    home stack added

---
+++ client/src/routes/HomeStack.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-shadow */
+import React from 'react';
+import {DeviceEventEmitter} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Statistics} from '../screens';
+import {TabIcon, Add} from '../components';
+import {colors, strings, images, perfectSize} from '../theme';
+import {hidden_bottom} from './config';
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
+import DashboardStack from './DashboardStack';
+import TransactionStack from './TransactionStack';
+
+const Tab = createBottomTabNavigator();
+
+export default function HomeStack() {
+  const getTabBarVisibility = route => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    if (hidden_bottom.includes(routeName)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showIcon: true,
+        showLabel: false,
+        lazyLoad: true,
+        safeAreaInsets: {
+          bottom: perfectSize(0),
+        },
+        style: {
+          backgroundColor: colors.tabBarBackgroundColor,
+          height: perfectSize(80),
+          borderTopWidth: perfectSize(0.5),
+          borderTopColor: 'rgba(255,255,255,0.1)',
+        },
+      }}>
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardStack}
+        options={({route}) => ({
+          tabBarIcon: props => (
+            <TabIcon
+              source={images.homeTab}
+              name={strings.tabBarLabels.home}
+              {...props}
+            />
+          ),
+          tabBarVisible: getTabBarVisibility(route),
+        })}
+      />
+      <Tab.Screen
+        name="Add"
+        component={TransactionStack}
+        options={props => ({
+          tabBarIcon: props => <Add {...props} />,
+          tabBarVisible: false,
+        })}
+      />
+      <Tab.Screen
+        name="Stat"
+        component={Statistics}
+        options={{
+          tabBarIcon: props => (
+            <TabIcon
+              source={images.statTab}
+              name={strings.tabBarLabels.stat}
+              {...props}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
