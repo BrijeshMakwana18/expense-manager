fe5dec65ec52c1eef9d9aaaad7d5ea39a4d5be78
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-04-24 01:22:11 +0530

    web scrapping added

---
+++ server/controllers/investments.js
@@ -0,0 +1,184 @@
+const router = require("express").Router();
+const axios = require("axios").default;
+const cheerio = require("cheerio");
+const authorization = require("./authorization");
+// const MF = "https://api.mfapi.in/mf";
+const yahooFinance = "https://finance.yahoo.com/quote/";
+let MFInvestments = [
+  { id: 135781, units: 286.576, avg: 34.89 },
+  { id: 120465, units: 2036.235, avg: 49.1 },
+  { id: 120847, units: 5.52, avg: 250.8213 },
+];
+let stocksInvestments = [
+  { id: "GOLDBEES.NS", units: 450, avg: 44.1 },
+  { id: "IRB.NS", units: 25, avg: 259.55 },
+  { id: "NIFTYBEES.NS", units: 200, avg: 179.96 },
+];
+router.post("/", authorization, async (req, res) => {
+  let stocks = [];
+
+  //Stocks
+  for (let i = 0; i < stocksInvestments.length; i++) {
+    await axios(yahooFinance + stocksInvestments[i].id).then((response) => {
+      let html = response.data;
+      const $ = cheerio.load(html);
+      let priceData = $("div > div > div > fin-streamer").html();
+      let priceChangeData = $(
+        "div > div > div > fin-streamer:nth-child(2) > span"
+      ).html();
+      let changePercentageData = $(
+        "div > div > div > fin-streamer:nth-child(3) > span"
+      ).html();
+      let nameData = $("div > div> div > h1").html();
+      let lastUpdatedData = $("#quote-market-notice > span").html();
+      //Calculation values
+      let units = stocksInvestments[i].units;
+      let investedNav = stocksInvestments[i].avg;
+      let currentNav = parseFloat(priceData.replace(/,/g, ""));
+      let priceChange = parseFloat(priceChangeData);
+      let pricePercentChange = parseFloat(
+        changePercentageData.replace(/%|[(]|[)]/g, "")
+      );
+      let investmentValue = investedNav * units;
+      let currentValue = currentNav * units;
+      let investmentValueChange = (currentValue - investmentValue).toFixed(2);
+      let navChange = {
+        price: priceChange,
+        percentage: `${pricePercentChange}%`,
+      };
+      let dailyStockPriceChange = priceChange * units;
+      let totalInvestmentChange = {
+        price: investmentValueChange,
+        percentage: `${(
+          (100 * investmentValueChange) /
+          investmentValue
+        ).toFixed(2)}%`,
+      };
+      let tempStocks = {
+        id: stocksInvestments[i].id,
+        name: nameData.toString(),
+        units: units,
+        investedNav: investedNav,
+        currentNav: currentNav,
+        investmentValue: investmentValue,
+        currentValue: currentValue,
+        lastUpdated: lastUpdatedData.toString(),
+        dailyPriceChange: dailyStockPriceChange,
+        navChange: navChange,
+        investmentChange: totalInvestmentChange,
+      };
+      stocks.push(tempStocks);
+    });
+  }
+  let totalStocksInvestment = stocks.reduce(
+    (acc, item) => acc + item.investmentValue,
+    0
+  );
+  let totalStocksValue = stocks.reduce(
+    (acc, item) => acc + item.currentValue,
+    0
+  );
+  let totalStocksPL = totalStocksValue - totalStocksInvestment;
+  let totalStocksPLPercentage = (
+    (100 * totalStocksPL) /
+    totalStocksInvestment
+  ).toFixed(2);
+  let dailyStockPriceChange = stocks.reduce(
+    (acc, item) => acc + item.dailyPriceChange,
+    0
+  );
+  let dailyStockPercentageChange = (
+    (100 * dailyStockPriceChange) /
+    totalStocksInvestment
+  ).toFixed(2);
+
+  let MF = [];
+  //MF
+  for (let i = 0; i < MFInvestments.length; i++) {
+    await axios
+      .get(`https://api.mfapi.in/mf/${MFInvestments[i].id}`)
+      .then((response) => {
+        if (response.data.status === "SUCCESS") {
+          //Mutual fund data
+          let meta = response.data.meta;
+          let data = response.data.data;
+
+          //Calculation values
+          let units = MFInvestments[i].units;
+          let investedNav = MFInvestments[i].avg;
+          let currentNav = parseFloat(data[0].nav);
+          let previousNav = data[1].nav;
+          let priceChange = (currentNav - previousNav).toFixed(2);
+          let investmentValue = investedNav * units;
+          let currentValue = currentNav * units;
+          let investmentValueChange = (currentValue - investmentValue).toFixed(
+            2
+          );
+          let dailyMFPriceChange = priceChange * units;
+          let navChange = {
+            price: priceChange,
+            percentage: `${((100 * priceChange) / previousNav).toFixed(2)}%`,
+          };
+          let investmentChange = {
+            price: investmentValueChange,
+            percentage: `${(
+              (100 * investmentValueChange) /
+              investmentValue
+            ).toFixed(2)}%`,
+          };
+          let temp = {
+            id: meta.scheme_code,
+            name: meta.scheme_name,
+            investedNav: investedNav,
+            currentNav: currentNav,
+            investmentValue: investmentValue,
+            currentValue: currentValue,
+            units: units,
+            lastUpdated: data[0].date,
+            dailyMFPriceChange: dailyMFPriceChange,
+            navChange: navChange,
+            investmentChange: investmentChange,
+          };
+          MF.push(temp);
+        }
+      });
+  }
+  let totalMFInvestment = MF.reduce(
+    (acc, item) => acc + item.investmentValue,
+    0
+  );
+  let totalMFValue = MF.reduce((acc, item) => acc + item.currentValue, 0);
+  let totalMFPL = totalMFValue - totalMFInvestment;
+  let totalMFPLPercentage = ((100 * totalMFPL) / totalMFInvestment).toFixed(2);
+  let dailyMFPriceChange = MF.reduce(
+    (acc, item) => acc + item.dailyMFPriceChange,
+    0
+  );
+  let dailyMFPercentageChange = (
+    (100 * dailyMFPriceChange) /
+    totalMFInvestment
+  ).toFixed(2);
+
+  res.send({
+    mutualFunds: {
+      totalMFInvestment: totalMFInvestment,
+      totalMFValue: totalMFValue,
+      totalMFPL: totalMFPL,
+      totalMFPLPercentage: `${totalMFPLPercentage}%`,
+      dailyPriceChange: dailyMFPriceChange.toFixed(2),
+      dailyPercentageChange: `${dailyMFPercentageChange}%`,
+      data: MF,
+    },
+    stocks: {
+      totalStocksInvestment: totalStocksInvestment,
+      totalStocksValue: totalStocksValue,
+      totalStocksPL: totalStocksPL,
+      totalStocksPLPercentage: `${totalStocksPLPercentage}%`,
+      dailyPriceChange: dailyStockPriceChange.toFixed(2),
+      dailyPercentageChange: `${dailyStockPercentageChange}%`,
+      data: stocks,
+    },
+  });
+});
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,12 @@ const login = require("../controllers/login");
 const transaction = require("../controllers/addTransaction");
 const fetchTransaction = require("../controllers/dashboard");
 const stat = require("../controllers/stat");
+const investments = require("../controllers/investments");
 
 router.use("/register", register);
 router.use("/login", login);
 router.use("/transaction", transaction);
 router.use("/dashboard", fetchTransaction);
 router.use("/stat", stat);
+router.use("/investments", investments);
 module.exports = router;
