4812fc3abd2b9dca4c9a62d5006bc5d49ac3817d
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-04-27 18:42:29 +0530

    app routes updated

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-shadow */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {store} from '../store';
 import AuthStack from './AuthStack';
-
-export default function Routes() {
+import HomeStack from './HomeStack';
+import {connect} from 'react-redux';
+function Routes(props) {
   return (
     <NavigationContainer>
-      <AuthStack />
+      {Object.keys(props.LoginReducer.user).length != 0 ? (
+        <HomeStack />
+      ) : (
+        <AuthStack />
+      )}
     </NavigationContainer>
   );
 }
+
+const mapStateToProps = state => {
+  return {LoginReducer: state.LoginReducer};
+};
+
+export default connect(mapStateToProps)(Routes);
