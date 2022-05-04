e1192274ed504db43c5fe68b5e39d4e3663666a3
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-05-04 20:08:51 +0530

    investment action added

--- client/src/screens/Home/actions.js
+++ client/src/screens/Home/actions.js
@@ -3,6 +3,10 @@ export const fetchDashboard = data => ({
   type: actionTypes.FETCH_DASHBOARD,
   payload: data,
 });
+export const fetchInvestments = data => ({
+  type: actionTypes.FETCH_INVESTMENTS,
+  payload: data,
+});
 export const setDashboardData = data => ({
   type: actionTypes.SET_DASHBOARD_DATA,
   payload: data,
