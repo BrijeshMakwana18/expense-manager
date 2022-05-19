b8929b2a9b8c5612a5a7500f7f88979ccaf8719d
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-05-19 19:07:25 +0530

    investments home saga

--- client/src/screens/Home/saga.js
+++ client/src/screens/Home/saga.js
@@ -1,6 +1,6 @@
 import {put, takeLatest} from 'redux-saga/effects';
 import {HTTP_METHODS} from '../../services/api-constants';
-import {FETCH_DASHBOARD} from '../../services/api-endpoints';
+import {FETCH_DASHBOARD, FETCH_INVESTMETNS} from '../../services/api-endpoints';
 import {request} from '../../services/services';
 import {actionTypes} from './actionTypes';
 function* fetchDashboard(action) {
@@ -10,7 +10,6 @@ function* fetchDashboard(action) {
       HTTP_METHODS.POST,
       action.payload.params,
       true,
-      action.payload.token,
     );
     if (response.data.responseType) {
       yield put({
@@ -29,4 +28,43 @@ function* fetchDashboard(action) {
 }
 export function* fetchDashboardWatcher() {
   yield takeLatest(actionTypes.FETCH_DASHBOARD, fetchDashboard);
+}
+
+function* fetchInvestments(action) {
+  yield put({
+    type: actionTypes.LOADING_INVESTMENTS,
+    payload: true,
+  });
+  try {
+    const {response} = yield request(
+      FETCH_INVESTMETNS(),
+      HTTP_METHODS.POST,
+      action.payload.params,
+      true,
+    );
+    if (response.data) {
+      yield put({
+        type: actionTypes.SET_INVESTMENTS_DATA,
+        payload: response.data,
+      });
+      yield put({
+        type: actionTypes.LOADING_INVESTMENTS,
+        payload: false,
+      });
+    }
+    if (action.payload.onSuccess) {
+      action?.payload?.onSuccess(response.data);
+    }
+  } catch (error) {
+    yield put({
+      type: actionTypes.LOADING_INVESTMENTS,
+      payload: false,
+    });
+    if (action.payload.onError) {
+      action?.payload?.onError(error.response.data);
+    }
+  }
+}
+export function* fetchInvestmentsWatcher() {
+  yield takeLatest(actionTypes.FETCH_INVESTMENTS, fetchInvestments);
 }
