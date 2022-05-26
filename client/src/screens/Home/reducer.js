569dcbc4cadbc08b55b5a2912c6801c7621c66ad
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-05-26 18:59:13 +0530

    app reducer changes

--- client/src/screens/Home/reducer.js
+++ client/src/screens/Home/reducer.js
@@ -2,6 +2,8 @@ import {actionTypes} from './actionTypes';
 const initialState = {
   userDetails: {},
   dashboardData: {},
+  investments: {},
+  isInvestmentsLoading: true,
 };
 
 const AppReducer = (state = initialState, action) => {
@@ -10,6 +12,13 @@ const AppReducer = (state = initialState, action) => {
       return {...state, userDetails: action.payload};
     case actionTypes.SET_DASHBOARD_DATA:
       return {...state, dashboardData: action.payload};
+    case actionTypes.LOADING_INVESTMENTS:
+      return {...state, isInvestmentsLoading: action.payload};
+    case actionTypes.SET_INVESTMENTS_DATA:
+      return {
+        ...state,
+        investments: action.payload,
+      };
     default:
       return state;
   }
